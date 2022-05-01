f3a87a91a0d751eab61a7bbfd22c3d0d28a71142
CT <tellezc007@gmail.com>
2022-05-01 16:03:33 -0700

    add middleware and requires

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
