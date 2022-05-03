6a5b0fdd2c5fd2ab770ea225e0cb1dfe22d7e827
CT <tellezc007@gmail.com>
2022-05-02 20:02:28 -0700

    move primary key and add proper syntax

--- db/schema.sql
+++ db/schema.sql
@@ -14,10 +14,10 @@ PRIMARY KEY (id)
 
 CREATE TABLE role (
 
-id INT AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL NOT NULL,
-department_id INT, 
+department_id INT,
 FOREIGN KEY (department_id)
 REFERENCES department(id)
 ON DELETE SET NULL
@@ -27,13 +27,14 @@ ON DELETE SET NULL
 
 CREATE TABLE employee (
 
-id INT AUTO_INCREMENT,
+id INT PRIMARY KEY AUTO_INCREMENT,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT,
-manager_id INT NOT NULL
+manager_id INT,
 FOREIGN KEY (role_id)
 REFERENCES role(id)
+ON DELETE SET NULL,
 FOREIGN KEY (manager_id)
 REFERENCES employee(id)
 ON DELETE SET NULL
