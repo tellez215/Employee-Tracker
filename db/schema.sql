6587385683bcacde647c7e91d56d8138511547df
CT <tellezc007@gmail.com>
2022-05-02 19:24:37 -0700

    add tables and add rows

--- db/schema.sql
+++ db/schema.sql
@@ -12,4 +12,30 @@ PRIMARY KEY (id)
 );
 
 
-CREATE TABLE role
+CREATE TABLE role (
+
+id INT AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT, 
+FOREIGN KEY (department_id)
+REFERENCES department(id)
+ON DELETE SET NULL
+    
+);
+
+
+CREATE TABLE employee (
+
+id INT AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT NOT NULL
+FOREIGN KEY (role_id)
+REFERENCES role(id)
+FOREIGN KEY (manager_id)
+REFERENCES employee(id)
+ON DELETE SET NULL
+
+);
