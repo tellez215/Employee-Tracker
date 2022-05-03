639284dcbf880c473b5d564955185f81dbbe3e19
CT <tellezc007@gmail.com>
2022-05-02 20:12:48 -0700

    add cases to display picked options

--- index.js
+++ index.js
@@ -41,13 +41,38 @@ function options() {
                 'Delete an employee',
                 'EXIT'
             ]
+        }).then(function(answer) {
+            switch (answer.action) {
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'View all departments':
+                    viewDepartments();
+                    break;
+                case 'View all roles':
+                    viewRoles();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Update employee role':
+                    updateRole();
+                    break;
+                case 'Delete an employee':
+                    deleteEmployee();
+                    break;
+                case 'EXIT':
+                    exitApp();
+                    break;
+            }
         })
-
-
-
-
-
-}
+};
 
 
 
